4bdc0572698bf6f3b5aa5c333945bd15bc7c8366
Dzakirin Asyraff <dzakirinasyraff@gmail.com>
2023-04-25 12:01:14 +0800

    Added cors option

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,7 +5,15 @@ import queueRoutes from "./routes/queue.routes";
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+    origin: [
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://blue-sand-0962c6400.3.azurestaticapps.net/",
+    ]
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded());
 
@@ -13,6 +21,9 @@ app.use("/api/counter", counterRoutes);
 app.use("/api/queue", queueRoutes);
 app.use("/", (req, res) => {
     res.send("Server running!");
+});
+app.use('/test', (req, res) => {
+    res.send("Test route");
 });
 
 export default app;
